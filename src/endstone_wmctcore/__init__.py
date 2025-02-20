a9e03d2b110ce01df7df336ef832efc15c61e326
PrimeStrat <74157166+PrimeStrat@users.noreply.github.com>
2025-02-19 22:35:14 -0500

    core files

---
+++ src/endstone_wmctcore/__init__.py
@@ -0,0 +1,3 @@
+from src.endstone_wmctcore.wmctcore import WMCTPlugin
+
+__all__ = ["WMCTPlugin"]
---
+++ src/endstone_wmctcore/wmctcore.py
@@ -0,0 +1,5 @@
+from endstone._internal.endstone_python import Plugin
+
+class WMCTPlugin(Plugin):
+    api_version = "0.6.0"
+    authors = ["PrimeStrat", "CraZ-Guy", "trainer jeo"]
