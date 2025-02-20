39004c5b544803a0225dd13d324a6390a3fc6243
PrimeStrat <74157166+PrimeStrat@users.noreply.github.com>
2025-02-19 22:36:09 -0500

    fixing crazy guy name kekw

--- src/endstone_wmctcore/wmctcore.py
+++ src/endstone_wmctcore/wmctcore.py
@@ -2,4 +2,4 @@ from endstone._internal.endstone_python import Plugin
 
 class WMCTPlugin(Plugin):
     api_version = "0.6.0"
-    authors = ["PrimeStrat", "CraZ-Guy", "trainer jeo"]
+    authors = ["PrimeStrat", "Cra-ZGuy", "trainer jeo"]
