56b15f6049cbeb7e147fd087f2a1c5ba1bb61a10
PrimeStrat <74157166+PrimeStrat@users.noreply.github.com>
2025-02-19 22:42:10 -0500

    file setup

--- src/endstone_wmctcore/wmctcore.py
+++ src/endstone_wmctcore/wmctcore.py
@@ -3,3 +3,5 @@ from endstone._internal.endstone_python import Plugin
 class WMCTPlugin(Plugin):
     api_version = "0.6.0"
     authors = ["PrimeStrat", "Cra-ZGuy", "trainer jeo"]
+
+    # Import processors here
