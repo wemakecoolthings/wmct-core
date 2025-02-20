4a95671d67f3b694392023297dfeb1073e5c5011
PrimeStrat <74157166+PrimeStrat@users.noreply.github.com>
2025-02-20 16:31:25 -0500

    create prefixUtil

---
+++ src/endstone_wmctcore/utils/prefixUtil.py
@@ -0,0 +1,11 @@
+from endstone._internal.endstone_python import ColorFormat
+class Prefix:
+
+    def infoLog(self):
+        return f"{ColorFormat.BOLD}{ColorFormat.DARK_GRAY}[{ColorFormat.YELLOW}>{ColorFormat.DARK_GRAY}] {ColorFormat.RESET}"
+
+    def modLog(self):
+        return f"{ColorFormat.BOLD}{ColorFormat.DARK_GRAY}[{ColorFormat.RED}ML{ColorFormat.DARK_GRAY}] {ColorFormat.RESET}"
+
+    def griefLog(self):
+        return f"{ColorFormat.BOLD}{ColorFormat.DARK_GRAY}[{ColorFormat.GOLD}GL{ColorFormat.DARK_GRAY}] {ColorFormat.RESET}"
