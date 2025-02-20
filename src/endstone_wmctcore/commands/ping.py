189d51dce0d1f8cf049e634aecb83d9320434f37
PrimeStrat <74157166+PrimeStrat@users.noreply.github.com>
2025-02-20 16:31:38 -0500

    test: ping command

---
+++ src/endstone_wmctcore/commands/ping.py
@@ -0,0 +1,51 @@
+from endstone._internal.endstone_python import CommandSender, Player, ColorFormat
+from src.endstone_wmctcore.utils.prefixUtil import Prefix
+
+class Ping:
+
+    # Declare Command
+    # Need to replace with custom selector
+    command: {
+         "ping": {
+            "description": "Check the server ping!",
+            "usages": ["/ping [target: player]"],
+            "permissions": ["wmctcore.command.ping"]
+        },
+    }
+
+    # Declare Permission Node
+    permission: {
+        "wmctcore.command.ping": {
+            "description": "Allows use of ping command",
+            "default": "true"
+        },
+    }
+
+    # Command Function
+    def run_command(self, sender: CommandSender, args: list[str]) -> bool:
+        if len(args) == 0:
+            if not isinstance(sender, Player):
+                sender.send_error_message("This command can only be executed by a player")
+                return False
+            target = sender
+        else:
+            player_name = args[0].strip('"')
+            if player_name == "@s":
+                target = sender
+            else:
+                target = self.server.get_player(player_name)
+                if target is None:
+                    sender.send_error_message(f"Player {player_name} not found.")
+                    return True
+
+        ping = target.ping
+        if ping <= 80:
+            ping_color = ColorFormat.GREEN
+        elif ping <= 160:
+            ping_color = ColorFormat.YELLOW
+        else:
+            ping_color = ColorFormat.RED
+
+        sender.send_message(
+            f"{Prefix.infoLog()}The ping of {target.name} is {ping_color}{ping}{ColorFormat.RESET}ms")
+        return True
