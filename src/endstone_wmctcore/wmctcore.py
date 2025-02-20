2bd83f23ed2115282fe7a158779deb0a9457fcc1
PrimeStrat <74157166+PrimeStrat@users.noreply.github.com>
2025-02-20 16:31:03 -0500

    init method

--- src/endstone_wmctcore/wmctcore.py
+++ src/endstone_wmctcore/wmctcore.py
@@ -4,4 +4,5 @@ class WMCTPlugin(Plugin):
     api_version = "0.6.0"
     authors = ["PrimeStrat", "Cra-ZGuy", "trainer jeo"]
 
-    # Import processors here
+    def __init__(self):
+        super().__init__()
