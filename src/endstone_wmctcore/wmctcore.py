cf649e2d8d6f49d79c51dd585f781b3bb7d5c50e
PrimeStrat <74157166+PrimeStrat@users.noreply.github.com>
2025-02-21 00:04:06 -0500

    general edits- no luck on plugin load yet

---
+++ src/endstone_wmctcore/wmctcore.py
@@ -0,0 +1,13 @@
+from endstone.plugin import Plugin
+
+class WMCTPlugin(Plugin):
+    api_version = "0.5"
+
+    def on_load(self) -> None:
+        self.logger.info("on_load is called!")
+
+    def on_enable(self) -> None:
+        self.logger.info("on_enable is called!")
+
+    def on_disable(self) -> None:
+        self.logger.info("on_disable is called!")
