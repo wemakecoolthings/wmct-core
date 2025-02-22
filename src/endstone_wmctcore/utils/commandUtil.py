e728b012c8386bb81832023d17b1f01c549fb8b3
YourPalMike <74157166+PrimeStrat@users.noreply.github.com>
2025-02-22 07:58:23 -0500

    Update commandUtil.py

--- src/endstone_wmctcore/utils/commandUtil.py
+++ src/endstone_wmctcore/utils/commandUtil.py
@@ -1,19 +1,18 @@
-class CommandRegister:
-    @staticmethod
-    def create_command(command_name: str, description: str, permissions: list):
-        command = {
-            command_name: {
-                "description": description,
-                "usages": [f"/{command_name}"],
-                "permissions": permissions
-            }
+def create_command(command_name: str, description: str, usages: list, permissions: list, aliases: list = None):
+    command = {
+        command_name: {
+            "description": description,
+            "usages": usages,
+            "permissions": permissions,
+            "aliases": aliases if aliases else []
         }
+    }
 
-        permission = {
-            permissions[0]: {  # Assuming the first permission is the main one
-                "description": f"Allows use of {command_name} command",
-                "default": "true"
-            }
+    permission = {
+        permissions[0]: {  # Assuming the first permission is the main one
+            "description": f"Allows use of the {command_name} command",
+            "default": "true"
         }
+    }
 
-        return command, permission
+    return command, permission
