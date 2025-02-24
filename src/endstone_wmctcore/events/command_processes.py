535e0017c4544372589bbd4af4d6a0f5186d2655
PrimeStrat <74157166+PrimeStrat@users.noreply.github.com>
2025-02-24 02:54:01 -0500

    /me crasher fix - w/o logger

--- src/endstone_wmctcore/events/command_processes.py
+++ src/endstone_wmctcore/events/command_processes.py
@@ -8,20 +8,13 @@ if TYPE_CHECKING:
 def handle_command_preprocess(self: "WMCTPlugin", event: PlayerCommandEvent):
     command = event.command
     player = event.player  # Get the player who issued the command
-    if command.count("@e") >= 5:
-        event.player.add_attachment(self, "minecraft.command.me", False)
-        event.is_cancelled = True
+    if command.startswith("/me") >= 1:
+        if command.count("@e") >= 5:
+            event.player.add_attachment(self, "minecraft.command.me", False)
+            event.is_cancelled = True
 
-        # Log the staff message
-        # TBD
+            # Log the staff message
+            # TBD
 
-        # Kick the player
-        player.kick("Crasher Detected")
-
-def handle_server_command_process(self: "WMCTPlugin", event: ServerCommandEvent):
-
-    # Dynamic Permissions Recalc
-    if event.command == "op":
-        self.reload_custom_perms()
-
-    return
+            # Kick the player
+            player.kick("Crasher Detected")
