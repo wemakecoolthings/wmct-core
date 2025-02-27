0ef02b0b4f164a64d25d90948d04c2c1ca2fafe0
PrimeStrat <74157166+PrimeStrat@users.noreply.github.com>
2025-02-27 17:23:39 -0500

    bossbar init and serverdata db

---
+++ src/endstone_wmctcore/commands/Core Commands/bossbar.py
@@ -0,0 +1,25 @@
+from endstone import Player
+from endstone.command import CommandSender
+from endstone_wmctcore.utils.commandUtil import create_command
+from endstone_wmctcore.utils.dbUtil import ServerDB
+from endstone_wmctcore.utils.prefixUtil import infoLog, errorLog
+
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from endstone_wmctcore.wmctcore import WMCTPlugin
+
+# Register command
+command, permission = create_command(
+    "bossbar",
+    "A bossbar manager!",
+    ["/bossbar"],
+    ["wmctcore.command.bossbar"]
+)
+
+# BOSSBAR COMMAND FUNCTIONALITY
+def handler(self: "WMCTPlugin", sender: CommandSender, args: list[str]) -> bool:
+
+    db = ServerDB("wmctcore_serverdata.db") # Example call for server data class
+
+    return True
