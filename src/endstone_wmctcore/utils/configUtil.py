965f633686718f98e2af3ddaeb75d4cf58e3e411
Jeo <jeotrainer2800@gmail.com>
2025-03-08 12:54:00 -0600

    Fix config gen/load for Linux

--- src/endstone_wmctcore/utils/configUtil.py
+++ src/endstone_wmctcore/utils/configUtil.py
@@ -2,7 +2,12 @@ import json
 import os
 import endstone_wmctcore
 
-CONFIG_PATH = os.path.join(os.path.dirname(endstone_wmctcore.__file__), '../../../../wmctcore-config.json')
+
+current_dir = os.path.dirname(os.path.abspath(__file__))
+for _ in range(4):
+    current_dir = os.path.dirname(current_dir)
+
+CONFIG_PATH = os.path.join(current_dir, 'wmctcore-config.json')
 
 def load_config():
     """Load or create a configuration file to enable/disable commands."""
@@ -14,9 +19,13 @@ def load_config():
             json.dump(default_config, config_file, indent=4)
 
     with open(CONFIG_PATH, "r") as config_file:
+        # Log in the console where the config is loaded from
+        print(f"[WMCT CORE] [DEBUG] Loaded config from {CONFIG_PATH}")
         return json.load(config_file)
 
 def save_config(config):
     """Save the current config state to disk."""
     with open(CONFIG_PATH, "w") as config_file:
+        # Log in the console  where the config saved
+        print(f"[WMCT CORE] [DEBUG] Saved config to {CONFIG_PATH}")
         json.dump(config, config_file, indent=4)
